d682ed6d2b16ebcad45d52fd5f6e48162548a73a
zhouxishun <1050494995@qq.com>
2021-01-04 22:28:07 +0800

    feat:gulp+webpack

---
+++ demo1/gulp.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // data: {
+  //   pkg: require('./package.json'),
+  //   date: new Date()
+  // },
+  // public: 'src/assets',
+  // paths: {
+  //   script: 'src/**',
+  //   pages: 'public',
+  //   public: 'src/assets',
+  // },
+}
